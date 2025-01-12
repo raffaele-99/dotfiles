620a6663f36661132b1599d4e74e34a7934c2f8a
luca-tanto <luca.fuda@tantosec.com>
2025-01-13 10:46:15 +1100

    update install

--- bash_custom/install/setup.sh
+++ bash_custom/install/setup.sh
@@ -1,15 +1,13 @@
 # for macOS
 
-# install xcode command line tools
-# xcode-select --install
+# first install nix
+# sh <(curl -L https://nixos.org/nix/install)
 
-
-# Install nix using determinate systems installer (has flakes enabled by default)
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+# then clone this repo
+# nix-shell -p git --run 'git clone https://github.com/raffaele-99/dotfiles.git ~/.dotfiles'
 
 # Install nix-darwin
-nix run nix-darwin -- switch --flake ~/.dotfiles/nix/darwin/nix/flake.nix\#tantobook
-
+nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/.dotfiles/nix/darwin/nix/flake.nix\#tantobook
 
 # Darwin rebuild
 # darwin-rebuild switch --flake ~/nix.\#simple
