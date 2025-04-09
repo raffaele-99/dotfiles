c8418a4b9560c82bf8657d0d86f4d53a0cbf58f1
luca <luca@hehe.com>
2025-04-09 14:43:50 +1000

    start init flake

---
+++ flake.nix
@@ -0,0 +1,60 @@
+{
+  description = "NixOS config, hopefully unified.";
+
+  inputs = {
+
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    darwin = {
+      url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+  };
+
+  outputs = { self, nixpkgs, home-manager, darwin, ... }: 
+  let
+    ];
+
+    mkSystem = import ./lib/mksystem.nix {
+      inherit overlays nixpkgs inputs;
+    };
+  in {
+    nixosConfigurations.vm-aarch64 = mkSystem "vm-aarch64" {
+      system = "aarch64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.vm-aarch64-prl = mkSystem "vm-aarch64-prl" rec {
+      system = "aarch64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.vm-aarch64-utm = mkSystem "vm-aarch64-utm" rec {
+      system = "aarch64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.vm-intel = mkSystem "vm-intel" rec {
+      system = "x86_64-linux";
+      user   = "mitchellh";
+    };
+
+    nixosConfigurations.wsl = mkSystem "wsl" {
+      system = "x86_64-linux";
+      user   = "mitchellh";
+      wsl    = true;
+    };
+
+    darwinConfigurations.macbook-pro-m1 = mkSystem "macbook-pro-m1" {
+      system = "aarch64-darwin";
+      user   = "mitchellh";
+      darwin = true;
+    };
+  };
+}
